a308bbdcdea5914277cda742fcb0eb92fd31f1aa
Dylan <macyvesdylan@gmail.com>
2022-04-25 22:25:13 +0700

    changes

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,7 @@
+import { AiFillInstagram, AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { FaKaggle } from 'react-icons/fa'
+
+
 
 function about() {
     return (
@@ -5,10 +9,10 @@ function about() {
             <div className="absolute top-1/4 left-1/2 -translate-x-1/2 text-center font-light">
                 <p className="text-white">This web is created by Dylan Mac Yves</p>
                 <div className="flex gap-x-2 justify-center text-white">
-                    <a className="hover:text-yellow-500 ease-in duration-100" target="_blank" href="https://www.instagram.com/sceeinc_002/"><i className="fab fa-instagram"></i></a>
-                    <a className="hover:text-yellow-500 ease-in duration-100" target="_blank" href="https://github.com/Dylanyves"><i className="fab fa-github"></i></a>
-                    <a className="hover:text-yellow-500 ease-in duration-100" target="_blank" href="https://www.linkedin.com/in/dylan-yves-7093741b3/"><i className="fab fa-linkedin"></i></a>
-                    <a className="hover:text-yellow-500 ease-in duration-100" target="_blank" href="https://www.kaggle.com/dylanyves"><i className="fab fa-kaggle"></i></a>
+                    <a className="hover:text-yellow-500 ease-in duration-100 text-xl" rel="noopener" href="https://www.instagram.com/sceeinc_002/"><AiFillInstagram /></a>
+                    <a className="hover:text-yellow-500 ease-in duration-100 text-xl" rel="noopener" href="https://github.com/Dylanyves"><AiFillGithub /></a>
+                    <a className="hover:text-yellow-500 ease-in duration-100 text-xl" rel="noopener" href="https://www.linkedin.com/in/dylan-yves-7093741b3/"><AiFillLinkedin /></a>
+                    <a className="hover:text-yellow-500 ease-in duration-100 text-xl" rel="noopener" href="https://www.kaggle.com/dylanyves"><FaKaggle /></a>
                 </div>
             </div>
         </div>
