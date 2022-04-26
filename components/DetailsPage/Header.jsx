969190afad94f126de4a1096d1bdc062fe4f01b8
Dylan <macyvesdylan@gmail.com>
2022-04-26 13:34:00 +0700

    fixed bugs

--- components/DetailsPage/Header.jsx
+++ components/DetailsPage/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { AiFillStar } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { BsGraphUp } from 'react-icons/bs'
 import { TYPE_VARS } from '/vars'
 
 function Header({ type, data }) {
@@ -9,16 +11,16 @@ function Header({ type, data }) {
                 <div className="flex justify-between items-center">
                     <h1 className="text-2xl tracking-tighter font-semibold mb-1">{type === TYPE_VARS.MOVIE ? data.original_title : data.name}</h1>
                     <div className="hidden sm:flex gap-x-6 self-end">
-                        <div className="relative before:font-light before:content-['Rating'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                            <i aria-hidden className="fas fa-star text-yellow-500 mr-1"></i>
+                        <div className=" flex gap-x-1 items-center relative before:font-light before:content-['Rating'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                            <AiFillStar className="text-yellow-500 text-sm " />
                             <span className="font-base">{data.vote_average}</span>
                         </div>
-                        <div className="relative before:font-light before:content-['Voters'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                            <i aria-hidden className="fas fa-user text-yellow-500 mr-1"></i>
+                        <div className="flex gap-x-1 items-center relative before:font-light before:content-['Voters'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                            <FaUser className="text-yellow-500 text-sm" />
                             <span className="font-base">{data.vote_count}</span>
                         </div>
-                        <div className="relative before:font-light before:content-['Popularity'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                            <i aria-hidden className="fas fa-chart-line text-blue-500 mr-1"></i>
+                        <div className="flex gap-x-1 items-center relative before:font-light before:content-['Popularity'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                            <BsGraphUp className="text-yellow-500 text-sm" />
                             <span className="font-base">{data.popularity}</span>
                         </div>
                     </div>
--- components/DetailsPage/Stats.jsx
+++ components/DetailsPage/Stats.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { BsGraphUp } from 'react-icons/bs'
 
 function Stats({ data }) {
     return (
-        <div className="flex justify-between items-center mb-10">
-            <div className="flex sm:hidden gap-x-6 self-end">
-                <div className="relative before:font-light before:content-['Rating'] before:text-[11px] before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                    <i aria-hidden className="text-sm fas fa-star text-yellow-500 mr-1"></i>
-                    <span className="font-base text-sm">{data.vote_average}</span>
-                </div>
-                <div className="relative before:font-light before:content-['Voters'] before:text-[11px] before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                    <i aria-hidden className="text-sm fas fa-user text-yellow-500 mr-1"></i>
-                    <span className="font-base text-sm">{data.vote_count}</span>
-                </div>
-                <div className="relative before:font-light before:content-['Popularity'] before:text-[11px] before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
-                    <i aria-hidden className="text-sm fas fa-chart-line text-blue-500 mr-1"></i>
-                    <span className="font-base text-sm">{data.popularity}</span>
-                </div>
+        <div className="flex sm:hidden gap-x-6 self-end mb-10">
+            <div className=" flex gap-x-1 items-center relative before:font-light before:content-['Rating'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                <AiFillStar className="text-yellow-500 text-sm " />
+                <span className="font-base">{data.vote_average}</span>
+            </div>
+            <div className="flex gap-x-1 items-center relative before:font-light before:content-['Voters'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                <FaUser className="text-yellow-500 text-sm" />
+                <span className="font-base">{data.vote_count}</span>
+            </div>
+            <div className="flex gap-x-1 items-center relative before:font-light before:content-['Popularity'] before:text-xs before:tracking-tighter before:text-gray-200 before:absolute before:top-[-13px] before:left-1/2 before:-translate-x-1/2">
+                <BsGraphUp className="text-yellow-500 text-sm" />
+                <span className="font-base">{data.popularity}</span>
             </div>
         </div>
     )
