bc8b2da4cef7412557bc258e6891242bb53db707
Dylan <macyvesdylan@gmail.com>
2022-04-26 16:34:22 +0700

    dynamic meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import Layout from "components/Layout/Layout"
 import { Helmet } from "react-helmet"
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+      <Helmet>
         <title>Movies Website</title>
         <meta charset="UTF-8" />
         <meta name="description" content="Search through hundreds of thousands movies information" />
@@ -15,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:image" content="/movies-website-1.png" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Helmet>
       <Layout genres="hello">
         <Component {...pageProps} />
       </Layout>
